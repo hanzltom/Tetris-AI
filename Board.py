27e09d6a0da84d9cc2bf0173c55a855629bbbba0
Tomas Hanzlik <tomhanzlik1@gmail.com>
2025-02-13 18:11:06 +0100

    fixed bug x y index

--- Board.py
+++ Board.py
@@ -1,29 +1,40 @@
+from objects.ObjectI import ObjectI
+from objects.ObjectJ import ObjectJ
+from objects.ObjectL import ObjectL
+from objects.ObjectO import ObjectO
+from objects.ObjectS import ObjectS
+from objects.ObjectT import ObjectT
+from objects.ObjectZ import ObjectZ
 from screen_setup import SCREEN_WIDTH, SCREEN_HEIGHT, x_boxes, y_boxes
 from Field import Field
 from Position import Position
 from colors import COLORS
+from objects import *
+import random
+
+object_list = [ObjectI, ObjectJ, ObjectL, ObjectO, ObjectS, ObjectT, ObjectZ]
 
 class Board:
     def __init__(self):
         self.board = [[]]
 
-        for x in range(x_boxes):
+        for y in range(y_boxes):
             self.board.append([])
-            for y in range(y_boxes):
+            for x in range(x_boxes):
 
                 x_cor = SCREEN_WIDTH / 11 * x
                 y_cor = SCREEN_HEIGHT / 21 * y
 
                 if x == 0 or x == 10 or y == 0 or y == 20:
-                    self.board[x].append(
+                    self.board[y].append(
                         Field(Position( x, y, (x_cor, y_cor), (x_cor + SCREEN_WIDTH / 11, y_cor + SCREEN_HEIGHT / 21)), False))
                 else:
-                    self.board[x].append(
+                    self.board[y].append(
                     Field(Position( x, y, (x_cor, y_cor), (x_cor + SCREEN_WIDTH / 11, y_cor + SCREEN_HEIGHT / 21)), True))
 
 
-    def print(self, pygame, screen):
-        [self.board[x][y].print(pygame, screen) for y in range(y_boxes) for x in range(x_boxes)]
+    def print_edges(self, pygame, screen):
+        [self.board[y][x].print(pygame, screen) for y in range(y_boxes) for x in range(x_boxes)]
 
     def print_lines(self, pygame, surface):
         # Vertical lines
@@ -37,5 +48,16 @@ class Board:
 
     def first_print(self, pygame, surface):
         surface.fill(COLORS["BLACK"])
-        self.print(pygame, surface)
+        self.print_edges(pygame, surface)
         self.print_lines(pygame, surface)
+
+    def check_collision(self):
+        return False
+
+    def create_object(self) -> bool :
+        chosen_object = random.choice(object_list)
+        new_object = chosen_object()
+        if self.check_collision():
+            return True
+
+        new_object.set_pos(x_boxes, y_boxes)
--- Game.py
+++ Game.py
@@ -1,16 +1,21 @@
 from Board import Board
+from colors import COLORS
+from screen_setup import *
 
 class Game:
     def __init__(self, pygame, surface):
         self.board = Board()
         self.board.first_print(pygame, surface)
 
+
     def loop(self, pygame, surface):
         running = True
+        new_object = None
         while running:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     running = False
 
+            #self.board.create_object()
             # Update the display
             pygame.display.flip()
