00bb52d918d173ce23f78eacc675c3c7c8052f0a
Tomas Hanzlik <tomhanzlik1@gmail.com>
2025-02-14 15:29:19 +0100

    added Field method

--- Field.py
+++ Field.py
@@ -7,7 +7,13 @@ class Field:
         self.color = "BLACK" if accessible else "GREY"
         self.pos = pos
 
+    def is_accessible(self):
+        return self.accessible
 
     def print(self, pygame, surface):
         pygame.draw.rect(surface, COLORS[self.color],
+                         pygame.Rect(self.pos.start_coordinates, self.pos.end_coordinates))
+
+    def print_object(self, pygame, surface, color):
+        pygame.draw.rect(surface, COLORS[color],
                          pygame.Rect(self.pos.start_coordinates, self.pos.end_coordinates))
