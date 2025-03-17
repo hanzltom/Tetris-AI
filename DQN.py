f4609248fcae551a98c419c2a66fdc331ac4e0d2
Tomas Hanzlik <tomhanzlik1@gmail.com>
2025-03-17 22:56:57 +0100

    added input size

--- DQN.py
+++ DQN.py
@@ -2,15 +2,16 @@ import torch
 import torch.nn as nn
 import torch.optim as optim
 import numpy as np
+import screen_setup
 
-
+input_size = screen_setup.x_boxes * screen_setup.y_boxes
 
 class DQN(nn.Module):
-    def __init__(self, input_size, output_size):
+    def __init__(self):
         super(DQN, self).__init__()
         self.fc1 = nn.Linear(input_size, 128)
         self.fc2 = nn.Linear(128, 128)
-        self.fc3 = nn.Linear(128, output_size)
+        self.fc3 = nn.Linear(128, 5)
 
     def forward(self, x):
         x = torch.relu(self.fc1(x))
