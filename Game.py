37f30a8aadadedc2ad5a78c9ae3a581e5809cf53
Tomas Hanzlik <tomhanzlik1@gmail.com>
2025-03-18 22:22:05 +0100

    game loop changed for nn

--- Game.py
+++ Game.py
@@ -1,8 +1,9 @@
 from Board import Board
 from colors import COLORS
 from screen_setup import *
-from Trainer import Trainer
+from Trainer import Trainer, board_to_tensor
 import time
+import numpy as np
 
 class Game:
     """
@@ -28,57 +29,41 @@ class Game:
         :return: None
         """
         try:
-            drop_interval = 1500  # Time in milliseconds for automatic downward movement
-            last_drop_time = pygame.time.get_ticks() # indicates how long it passed from the last drop of the object
-            new_object = None
-            running = True
-            create_new_object = True # if needed to create new object
-            while running:
-                current_time = pygame.time.get_ticks()
+            episodes = 1000
+            EPSILON = 1
+            for episode in range(episodes):
+                new_object = None
+                running = True
+                create_new_object = True # if needed to create new object
+                while running:
 
-                if self.board.is_out(): # if there is any object outside of zone
-                    break
+                    if self.board.is_out(): # if there is any object outside of zone
+                        break
 
-                if create_new_object:
-                    create_new_object = False
-                    object_valid, new_object = self.board.create_object()
-                    while not object_valid: # Object could not be created, creating different objects
+                    if create_new_object:
+                        create_new_object = False
                         object_valid, new_object = self.board.create_object()
+                        while not object_valid: # Object could not be created, creating different objects
+                            object_valid, new_object = self.board.create_object()
+                        state = board_to_tensor(self.board.board, new_object)
 
-                # Print board and object
-                self.board.print_board(pygame, surface, new_object)
+                    # Print board and object
+                    self.board.print_board(pygame, surface, new_object)
 
-                # Get input
-                for event in pygame.event.get():
-                    if event.type == pygame.QUIT:
-                        running = False
+                    # Select action using ε-greedy policy
+                    if np.random.rand() < EPSILON:
+                        action = np.random.randint(0, 5)  # Random action
+                    else:
+                        action = self.trainer.get_action(state)
 
-                # Allow multiple moves in one frame
-                for _ in range(5):  # Adjust the number of moves per frame
-                    action = self.trainer.train(self.board.board, new_object)
-                    if action == 0:
-                        self.board.move_object(new_object, "LEFT")
-                    elif action == 1:
-                        self.board.move_object(new_object, "RIGHT")
-                    elif action == 2:
-                        self.board.move_object(new_object, "DOWN")
-                    elif action == 3:
-                        self.board.rotate_piece(new_object, "RIGHT")
-                    elif action == 4:
-                        self.board.rotate_piece(new_object, "LEFT")
-                    time.sleep(0.1)
+                    reward, done, lock_object = self.board.apply_action(new_object, action)
+                    next_state = board_to_tensor(self.board.board, new_object)
 
-                # If enough time passed, drop the object
-                if current_time - last_drop_time > drop_interval:
-                    if not self.board.move_object(new_object, "DOWN"): # If we cannot drop it further
+                    if lock_object:
                         self.board.lock_object(new_object)  # Lock object on the board
-                        self.board.clear_lines(pygame, surface) # Clear full lines
+                        self.board.clear_lines(pygame, surface)  # Clear full lines
                         create_new_object = True
-                    last_drop_time = current_time
 
-
-
-            input("Enter for exit")
 
 
         except Exception as e:
