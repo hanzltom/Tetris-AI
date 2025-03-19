38a248ca209a8e9161be9cae5ac91131b8d8ae43
Tomas Hanzlik <tomhanzlik1@gmail.com>
2025-03-20 00:08:31 +0100

    added exception for keyboard interrupt

--- window.py
+++ window.py
@@ -3,21 +3,26 @@ import sys
 import os
 from Game import Game
 from screen_setup import SCREEN_WIDTH, SCREEN_HEIGHT
+import torch
 
+try:
+    # Initialize Pygame
+    pygame.init()
 
-# Initialize Pygame
-pygame.init()
+    # Set up window position
+    os.environ['SDL_VIDEO_WINDOW_POS'] = "%d,%d" % (400,0)
+    # Set up window dimensions
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    pygame.display.set_caption("Tetris")
 
-# Set up window position
-os.environ['SDL_VIDEO_WINDOW_POS'] = "%d,%d" % (400,0)
-# Set up window dimensions
-screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-pygame.display.set_caption("Tetris")
+    # Start Game
+    game = Game(pygame, screen)
+    game.loop(pygame, screen)
 
-# Start Game
-game = Game(pygame, screen)
-game.loop(pygame, screen)
+    # Quit Game
+    pygame.quit()
+    sys.exit()
 
-# Quit Game
-pygame.quit()
-sys.exit()
+except KeyboardInterrupt as e:
+    print(e)
+    torch.save(game.trainer.model.state_dict(), "tetris_dqn.pth")
