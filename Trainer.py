137f28ee8f1e5730c18eb4ca031b5d73d904c250
Tomas Hanzlik <tomhanzlik1@gmail.com>
2025-04-06 13:53:55 +0200

    comments

--- Trainer.py
+++ Trainer.py
@@ -10,7 +10,13 @@ import random
 
 
 class Trainer:
+    """
+    Class Trainer for training the neural network
+    """
     def __init__(self):
+        """
+        Constructor creating the neural network
+        """
         self.model = DQN()
         if int(input("Do you want to load the model? 1/0: ")) == 1:
             print("Using saved model")
@@ -18,20 +24,33 @@ class Trainer:
 
 
     def get_action(self, state_tensor):
-        q_values = self.model(state_tensor)  # Predict Q-values
+        """
+        Function to get next move (action) from the model
+        :param state_tensor: environment state
+        :return: best move
+        """
+        q_values = self.model(state_tensor)  # predict q-values
 
         action = torch.argmax(q_values).item()
         return action
 
     def train(self, replay_memory, BATCH_SIZE, GAMMA):
+        """
+        Function to train the neural network
+        :param replay_memory: Memory with previous states, its next move and its reward
+        :param BATCH_SIZE: Sample size taken from replay_memory
+        :param GAMMA: Discount factor
+        """
         optimizer = optim.Adam(self.model.parameters(), lr=0.001)
 
         if len(replay_memory) < BATCH_SIZE:
             return # Not enough data in memory
 
+        # Get random samples
         batch = random.sample(replay_memory, BATCH_SIZE)
         states, actions, rewards, next_states, dones = zip(*batch)
 
+        # Stack concatenates a list of tensors along a new dimension
         states = torch.stack(states)
         next_states = torch.stack(next_states)
         actions = torch.tensor(actions, dtype=torch.long).unsqueeze(1)
@@ -50,6 +69,12 @@ class Trainer:
         optimizer.step()
 
 def board_to_tensor(board, object = None):
+    """
+    Function to convert a board to a tensor
+    :param board: Board object
+    :param object: Last object on the board (object is fixed on the board ones it is at the bottom)
+    :return: Tensor board
+    """
     if object:
         board_list = [
             [0 if board[y][x].accessible == 0 or (x, y) in object.pos else 1 for x in range(screen_setup.x_boxes)] for y
@@ -59,7 +84,7 @@ def board_to_tensor(board, object = None):
             [0 if board[y][x].accessible == 0  else 1 for x in range(screen_setup.x_boxes)]
             for y in range(screen_setup.y_boxes)]
 
-    numpy_array = np.array(board_list, dtype=np.float32)
-    tensor_board = torch.tensor(numpy_array.flatten())
+    numpy_array = np.array(board_list, dtype=np.float32) # convert to numpy
+    tensor_board = torch.tensor(numpy_array.flatten()) # conver to tensor
     return tensor_board
 
