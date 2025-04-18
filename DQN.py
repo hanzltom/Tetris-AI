64f8a08fe47420bd3e7aeb8cfa8399d7fca0e74a
Tomas Hanzlik <tomhanzlik1@gmail.com>
2025-04-18 11:24:22 +0200

    changed NN Linear layer to convolutional layers

--- DQN.py
+++ DQN.py
@@ -12,11 +12,15 @@ class DQN(nn.Module):
     """
     def __init__(self):
         super(DQN, self).__init__()
-        self.fc1 = nn.Linear(input_size, 128)
-        self.fc2 = nn.Linear(128, 128)
-        self.fc3 = nn.Linear(128, 5)
+        self.conv1 = nn.Conv2d(1, 32, kernel_size=3, padding=1)
+        self.conv2 = nn.Conv2d(32, 64, kernel_size=3, padding=1)
+
+        self.fc1 = nn.Linear(64 * 21 * 11, 128)
+        self.fc2 = nn.Linear(128, 5)
 
     def forward(self, x):
+        x = torch.relu(self.conv1(x))
+        x = torch.relu(self.conv2(x))
+        x = x.view(x.size(0), -1)  # flatten
         x = torch.relu(self.fc1(x))
-        x = torch.relu(self.fc2(x))
-        return self.fc3(x)  # Q-values for each action
+        return self.fc2(x)
